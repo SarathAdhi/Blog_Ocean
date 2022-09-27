af6fade0617249b96417688f650b134a000d400b
SarathAdhi <s.sarathadhithya@gmail.com>
2022-09-27 20:28:16 +0530

    minor changes

---
+++ src/pages/questions/index.tsx
@@ -0,0 +1,12 @@
+import PageLayout from "@layouts/PageLayout";
+import React from "react";
+
+const Questions = () => {
+  return (
+    <PageLayout title="">
+      <div>Questions</div>
+    </PageLayout>
+  );
+};
+
+export default Questions;
